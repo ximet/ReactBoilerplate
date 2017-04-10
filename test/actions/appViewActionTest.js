bd0b8cedc5754271a66c1caf3fe4a2f66fed022f
ximet <dq.ximet@gmail.com>
2017-04-10 19:48:43 +0300

    update in formating test

--- test/actions/appViewActionTest.js
+++ test/actions/appViewActionTest.js
@@ -1,45 +1,47 @@
 import expect from 'expect';
 import { SET_ACTIVE_VIEW, appViewActions } from '../../src/actions/appViewActions';
 
-describe('actionSetActiveStringView', () => {
-    it('should set active view with correct string', () => {
-        const viewName = 'TestView';
-        const expectedAction = {
-            type: SET_ACTIVE_VIEW,
-            values: {
-                activeViewName: viewName
-            }
-        };
+describe('actionSetActiveView', () => {
+    describe('actionSetActiveStringView', () => {
+        it('should set active view with correct string', () => {
+            const viewName = 'TestView';
+            const expectedAction = {
+                type: SET_ACTIVE_VIEW,
+                values: {
+                    activeViewName: viewName
+                }
+            };
 
-        expect(appViewActions[SET_ACTIVE_VIEW](viewName)).toEqual(expectedAction);
+            expect(appViewActions[SET_ACTIVE_VIEW](viewName)).toEqual(expectedAction);
+        });
     });
-});
 
 
-describe('actionSetActiveNumberView', () => {
-    it('should set active view', () => {
-        const viewName = 0;
-        const expectedAction = {
-            type: SET_ACTIVE_VIEW,
-            values: {
-                activeViewName: viewName
-            }
-        };
+    describe('actionSetActiveNumberView', () => {
+        it('should set active view', () => {
+            const viewName = 0;
+            const expectedAction = {
+                type: SET_ACTIVE_VIEW,
+                values: {
+                    activeViewName: viewName
+                }
+            };
 
-        expect(appViewActions[SET_ACTIVE_VIEW](viewName)).toEqual(expectedAction);
+            expect(appViewActions[SET_ACTIVE_VIEW](viewName)).toEqual(expectedAction);
+        });
     });
-});
 
-describe('actionSetActiveUndefinedView', () => {
-    it('should set active view', () => {
-        const viewName = undefined;
-        const expectedAction = {
-            type: SET_ACTIVE_VIEW,
-            values: {
-                activeViewName: viewName
-            }
-        };
+    describe('actionSetActiveUndefinedView', () => {
+        it('should set active view', () => {
+            const viewName = undefined;
+            const expectedAction = {
+                type: SET_ACTIVE_VIEW,
+                values: {
+                    activeViewName: viewName
+                }
+            };
 
-        expect(appViewActions[SET_ACTIVE_VIEW](viewName)).toEqual(expectedAction);
+            expect(appViewActions[SET_ACTIVE_VIEW](viewName)).toEqual(expectedAction);
+        });
     });
 });
